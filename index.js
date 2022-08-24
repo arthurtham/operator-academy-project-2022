ad7b1b3c9ddb0121cc922ca6529ad0206737a99f
Arthur Tham <19481191+arthurtham@users.noreply.github.com>
2022-08-24 00:11:34 -0700

    Revert "Ensure twilio library is referenced only once, in index"
    This reverts commit 6b5b16ad60119b537fae557e4bc755a2bcc0a1f7.

--- index.js
+++ index.js
@@ -17,5 +17,9 @@ app.use("/", routes);
 
 // Start the server
 app.listen(port, () => {
+  /*
+  This is an example of string interpolation via Template Literals.
+  This is an easier way of building strings than we've seen in the JS Test Lab. Read more: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
+  */
   console.log(`Your Express application is running on port ${port}`);
 });
--- src/routes.js
+++ src/routes.js
@@ -5,9 +5,9 @@
 const express = require("express");
 const router = express.Router();
 
-// const accountSid = process.env.TWILIO_ACCOUNT_SID;
-// const authToken = process.env.TWILIO_AUTH_TOKEN;
-// const client = require('twilio')(accountSid, authToken);
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
 
 const JServiceState = require("./state.js");
 let states = new JServiceState();
